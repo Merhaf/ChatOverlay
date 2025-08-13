b0a7209022f89ce156118820c13ec5cb60a69bd4
Merhaf <54317788+Merhaf@users.noreply.github.com>
2025-08-13 09:49:16 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
-const HOST = window.location.host;
-const socket = new ReconnectingWebSocket(`ws://${HOST}/ws`);
+const app = Vue.createApp({ // 실시간 반응형 인터페이스
+  data() {
+    return {
+      isPlayingOrWatching: false, // isPlayingOrWatching변수 초기값 지정
+    };
+  },
+  mounted() {
+const socket = new ReconnectingWebSocket(`ws://${window.location.host}/ws`); // 게임 웹소켓 연결
 
-let isPlayingOrWatching = false;
+    socket.onmessage = (event) => { // 서버에서 받은 데이터 처리
+      try {
+        const data = JSON.parse(event.data); // 서버에서 받은 데이터 json 형식으로
+		// console.log('받은 데이터:', data);  // 이거 활성화하면 인게임 데이터 로컬웹 콘솔창에 발사함
+        const isPlayingNow = (data.menu?.state === 2); // menu.state 값이 2라면 게임플레이 중
 
-socket.onmessage = (event) => {
-  try {
-    const data = JSON.parse(event.data); // 인게임 데이터 문자열을 JSON 형식으로 데이터 변경하기
-    // console.log('받은 데이터:', data);  // 이거 활성화하면 인게임 데이터 로컬웹 콘솔창에 발사함
-    const gamestate = data.menu?.state; // 인게임 상태 데이터를 gamestate 변수에 넣기
-    const isPlayingNow = (gamestate === 2); // gamestate가 2면 플레이 중(true), 아니면 false
+        if (isPlayingNow !== this.isPlayingOrWatching) { // 이전에 저장된 상태(isPlayingOrWatching)와 현재 상태(isPlayingNow)가 다를 때
+          this.isPlayingOrWatching = isPlayingNow; //  isPlayingOrWatching 값에 isPlayingNow를 넣음
+        }
+      } catch (e) {}
+    };
+  },
+});
 
-    if (isPlayingNow !== isPlayingOrWatching) { // isPlayingNow 값이랑 isPlayingOrWatching 비교해서 다르면
-      isPlayingOrWatching = isPlayingNow; // isPlayingOrWatching에다가 isPlayingNow의 값 집어넣기
-
-      // 전역 변수에 저장해서 Vue 등에서 접근 가능하게
-      window.isPlayingOrWatching = isPlayingOrWatching;
-    }
-  } catch (e) {}
-};
+app.mount('#overlay');
